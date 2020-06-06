7e0fa385b52d8c7795a9ef88b97e0a3c1a739310
William Schrader <wfschrad@gmail.com>
2020-06-06 14:40:17 -0700

    Add GraphQL functionality for 'manufacturers' and 'manufacturer' queries.

--- app/schema.py
+++ app/schema.py
@@ -1,5 +1,8 @@
 import graphene
+from graphene import relay
 from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType
+
+from .models import Manufacturer
 
 # class TestObj(graphene.ObjectType):
 #     title = graphene.String()
@@ -7,13 +10,30 @@ from graphene_sqlalchemy import SQLAlchemyConnectionField, SQLAlchemyObjectType
 #     age = graphene.Int()
 
 
-class Query(graphene.ObjectType):
-    hello = graphene.String(name=graphene.Argument(
-        graphene.String, default_value='stranger'),
-        age=graphene.Argument(graphene.Int))
+class ManufacturerType(SQLAlchemyObjectType):
+    class Meta:
+        model = Manufacturer
+        interfaces = (relay.Node, )
 
-    def resolve_hello(self, info, **args):
-        return f'Hello {args["name"]}, you are {args["age"]} years old!'
+
+class Query(graphene.ObjectType):
+    node = relay.Node.Field()
+    # manufacturers = SQLAlchemyConnectionField(Manufacturer.connection)
+    manufacturers = graphene.List(ManufacturerType)
+    manufacturer = graphene.Field(
+        ManufacturerType, manufacturer_id=graphene.Int())
+
+    def resolve_manufacturers(self, info, **kwargs):
+        return Manufacturer.query.all()
+
+    def resolve_manufacturer(self, info, manufacturer_id):
+        return Manufacturer.query.get(manufacturer_id)
+    # hello = graphene.String(name=graphene.Argument(
+    #     graphene.String, default_value='stranger'),
+    #     age=graphene.Argument(graphene.Int))
+
+    # def resolve_hello(self, info, **args):
+    #     return f'Hello {args["name"]}, you are {args["age"]} years old!'
 
 
 schema = graphene.Schema(query=Query)

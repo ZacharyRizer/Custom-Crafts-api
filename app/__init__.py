9d1ae0a326b5b5ce4c2228d2ff783309ceb48170
Zachary Rizer <zacharyrizer@gmail.com>
2020-06-07 09:02:02 -0700

    Auth0 front/back integration

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,48 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask_cors import cross_origin, CORS
+from flask_migrate import Migrate
 from .config import Configuration
 from .models import db
 from .routes import customers, ships
-from flask_migrate import Migrate
+from .auth import AuthError, requires_auth
 
 app = Flask(__name__)
 app.config.from_object(Configuration)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
 app.register_blueprint(customers.bp)
 app.register_blueprint(ships.bp)
 db.init_app(app)
 Migrate(app, db)
+
+# Error Handler
+
+
+@app.errorhandler(AuthError)
+def handle_auth_error(ex):
+    response = jsonify(ex.error)
+    response.status_code = ex.status_code
+    return response
+
+
+# This doesn't need authentication
+
+
+@app.route("/api/public")
+@cross_origin(headers=["Content-Type", "Authorization"])
+def public():
+    response = "Hello from a public endpoint! You don't need to be authenticated to see this."
+    return jsonify(message=response)
+
+
+# This needs authentication
+
+
+@app.route("/api/private")
+@cross_origin(headers=["Content-Type", "Authorization"])
+@requires_auth
+def private():
+    response = "Hello from a private endpoint! You need to be authenticated to see this."
+    return jsonify(message=response)
+
+

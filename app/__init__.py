d7a3edb2d2f5e645c7d803478159ebef3a3ef1d1
William Schrader <wfschrad@gmail.com>
2020-06-06 22:33:15 -0700

    Add cors.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_graphql import GraphQLView
+from flask_cors import CORS
 from .schema import schema
 
 from .config import Configuration
@@ -8,6 +9,7 @@ from .routes import customers, ships, gql
 from flask_migrate import Migrate
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object(Configuration)
 # app.register_blueprint(customers.bp)
 # app.register_blueprint(ships.bp)

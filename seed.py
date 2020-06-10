1b5f7374a8bca017c5edb2dbdd6aa31c5e99c5e1
Zachary Rizer <zacharyrizer@gmail.com>
2020-06-10 13:35:57 -0700

    database fixes

--- seed.py
+++ seed.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # size 1 = 100, 100-500, 500-1000, 1000-5000, 5000+
-# price = 10000, 10000-100000, 100000-500000, 500000-1000000, 1000000+
+# price = 100, 100-1000, 1000-10000, 10000-100000, 100000+
 # range = 25, 25-100, 100-1000, 1000-10000, 10000+
 # crew_cap = 10, 10-100, 100-500, 500-1000, 1000+
 
@@ -36,12 +36,12 @@ with app.app_context():
         model_link='/spaceships/test_ship.glb',
         description='Big black circle, causes destruction',
         stock=3,
-        price=47000000
+        price=470000
     )
     ship2 = Ship(
         name='SS Dreadnought',
         manufacturer_id=1,
-        category_id=1,
+        category_id=2,
         size=4788,
         designer='Glama V\'Leeni',
         crew_cap=1000,
@@ -51,12 +51,12 @@ with app.app_context():
         model_link='/spaceships/test_ship.glb',
         description='Big black circle, causes destruction',
         stock=3,
-        price=8500000
+        price=850000
     )
     ship3 = Ship(
         name='Despot',
         manufacturer_id=1,
-        category_id=1,
+        category_id=3,
         size=8993,
         designer='Kr\'Andull',
         crew_cap=500,
@@ -66,12 +66,12 @@ with app.app_context():
         model_link='/spaceships/test_ship.glb',
         description='Big black circle, causes destruction',
         stock=3,
-        price=15000000
+        price=150000
     )
     ship4 = Ship(
         name='Star Talon',
         manufacturer_id=2,
-        category_id=2,
+        category_id=4,
         size=340,
         designer='Aren Bellee',
         crew_cap=350,
@@ -81,12 +81,12 @@ with app.app_context():
         model_link='/spaceships/test_ship.glb',
         description='Big black circle, causes destruction',
         stock=3,
-        price=870000
+        price=8700
     )
     ship5 = Ship(
         name='Proton',
         manufacturer_id=2,
-        category_id=2,
+        category_id=5,
         size=945,
         designer='Zach Rizer',
         crew_cap=10,
@@ -96,37 +96,37 @@ with app.app_context():
         model_link='/spaceships/test_ship.glb',
         description='Big black circle, causes destruction',
         stock=3,
-        price=954000
+        price=954
     )
     ship6 = Ship(
         name='Trenxal',
         manufacturer_id=2,
-        category_id=2,
+        category_id=1,
         size=86,
         designer='Vlegi Moveloi',
         crew_cap=50,
-        travel_range=359,
+        travel_range=90,
         ftl=False,
         used=True,
         model_link='/spaceships/test_ship.glb',
         description='Big black circle, causes destruction',
         stock=3,
-        price=657000
+        price=657
     )
     ship7 = Ship(
         name='The Rubber Ducky',
         manufacturer_id=3,
-        category_id=3,
+        category_id=2,
         size=10,
         designer='Ian Magenta',
         crew_cap=2,
-        travel_range=25,
+        travel_range=23,
         ftl=True,
         used=False,
         model_link='/spaceships/test_ship.glb',
         description='Big black circle, causes destruction',
         stock=3,
-        price=3500
+        price=75
     )
     ship8 = Ship(
         name='The Æthelwulf',
@@ -135,18 +135,18 @@ with app.app_context():
         size=1212,
         designer='Take Mayuma',
         crew_cap=50,
-        travel_range=3,
+        travel_range=10,
         ftl=False,
         used=True,
         model_link='/spaceships/test_ship.glb',
         description='Big black circle, causes destruction',
         stock=3,
-        price=670000
+        price=67000
     )
     ship9 = Ship(
         name='Poseidon',
         manufacturer_id=3,
-        category_id=3,
+        category_id=4,
         size=7564,
         designer='Sui Gwu-Pao',
         crew_cap=1200,
@@ -156,12 +156,12 @@ with app.app_context():
         model_link='/spaceships/test_ship.glb',
         description='Big black circle, causes destruction',
         stock=3,
-        price=890000
+        price=8900
     )
     ship10 = Ship(
         name='Apollo',
         manufacturer_id=4,
-        category_id=4,
+        category_id=5,
         size=765,
         designer='William Schraeder',
         crew_cap=300,
@@ -171,12 +171,12 @@ with app.app_context():
         model_link='/spaceships/test_ship.glb',
         description='Big black circle, causes destruction',
         stock=3,
-        price=435000
+        price=435
     )
     ship11 = Ship(
         name='Oberon Nova',
         manufacturer_id=4,
-        category_id=4,
+        category_id=1,
         size=276,
         designer='Valdis Simril',
         crew_cap=100,
@@ -186,12 +186,12 @@ with app.app_context():
         model_link='/spaceships/test_ship.glb',
         description='Big black circle, causes destruction',
         stock=3,
-        price=87000
+        price=8700
     )
     ship12 = Ship(
         name='ISV Rimward Gold',
         manufacturer_id=4,
-        category_id=4,
+        category_id=2,
         size=764,
         designer='Guinevere Watrous',
         crew_cap=250,
@@ -206,7 +206,7 @@ with app.app_context():
     ship13 = Ship(
         name='Rimward Perseus',
         manufacturer_id=5,
-        category_id=5,
+        category_id=3,
         size=476,
         designer='Derrik Alder',
         crew_cap=100,
@@ -216,12 +216,12 @@ with app.app_context():
         model_link='/spaceships/test_ship.glb',
         description='Big black circle, causes destruction',
         stock=3,
-        price=9999
+        price=9000
     )
     ship14 = Ship(
         name='Tycho',
         manufacturer_id=5,
-        category_id=5,
+        category_id=4,
         size=156,
         designer='Maddox Volante',
         crew_cap=50,
@@ -246,7 +246,7 @@ with app.app_context():
         model_link='/spaceships/test_ship.glb',
         description='Big black circle, causes destruction',
         stock=3,
-        price=956000
+        price=999999
     )
 
     db.session.add(man1)

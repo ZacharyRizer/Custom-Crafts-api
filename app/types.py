1fe4d7b5f6ce40e09ce57805c94a4cda4d3cb3a2
Zachary Rizer <zacharyrizer@gmail.com>
2020-06-10 09:19:15 -0700

    removed relay nodes from GQL types

--- app/types.py
+++ app/types.py
@@ -7,41 +7,33 @@ from .models import db, Category, Customer, Manufacturer, Ship, Order, OrderItem
 class ShipType(SQLAlchemyObjectType):
     class Meta:
         model = Ship
-        interfaces = (relay.Node, )
 
 
 class CategoryType(SQLAlchemyObjectType):
     class Meta:
         model = Category
-        interfaces = (relay.Node, )
 
 
 class CustomerType(SQLAlchemyObjectType):
     class Meta:
         model = Customer
-        # Research following line, possibly unnecessary in all _Type Classes.
-        interfaces = (relay.Node, )
 
 
 class ManufacturerType(SQLAlchemyObjectType):
     class Meta:
         model = Manufacturer
-        interfaces = (relay.Node, )
 
 
 class OrderType(SQLAlchemyObjectType):
     class Meta:
         model = Order
-        interfaces = (relay.Node, )
 
 
 class OrderItemType(SQLAlchemyObjectType):
     class Meta:
         model = OrderItem
-        interfaces = (relay.Node, )
 
 
 class ReviewType(SQLAlchemyObjectType):
     class Meta:
         model = Review
-        interfaces = (relay.Node, )

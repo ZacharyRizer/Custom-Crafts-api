e56c50a17c1d0d0e5371beb61318e7aa6e71a011
Zachary Rizer <zacharyrizer@gmail.com>
2020-06-12 17:58:06 -0700

    add procfile

--- app/__init__.py
+++ app/__init__.py
@@ -8,46 +8,49 @@ from flask_graphql import GraphQLView
 from .schema import schema
 
 
-app = Flask(__name__)
-app.config.from_object(Configuration)
-cors = CORS(app, resources={r"/*": {"origins": "*"}})
-db.init_app(app)
-Migrate(app, db)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Configuration)
+    cors = CORS(app, resources={r"/*": {"origins": "*"}})
+    db.init_app(app)
+    Migrate(app, db)
 
 
-app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql',
-                                                           schema=schema,
-                                                           graphiql=True))
+    app.add_url_rule('/graphql', view_func=GraphQLView.as_view('graphql',
+                                                            schema=schema,
+                                                            graphiql=True))
 
-# Error Handler
+    # Error Handler
 
 
-@app.errorhandler(AuthError)
-def handle_auth_error(ex):
-    response = jsonify(ex.error)
-    response.status_code = ex.status_code
-    return response
+    @app.errorhandler(AuthError)
+    def handle_auth_error(ex):
+        response = jsonify(ex.error)
+        response.status_code = ex.status_code
+        return response
+
+    return app
 
 
 # This doesn't need authentication
 
 
-@app.route("/api/public")
-@cross_origin(headers=["Content-Type", "Authorization"])
-def public():
-    response = "Hello from a public endpoint! You don't need to be authenticated to see this."
-    return jsonify(message=response)
+# @app.route("/api/public")
+# @cross_origin(headers=["Content-Type", "Authorization"])
+# def public():
+#     response = "Hello from a public endpoint! You don't need to be authenticated to see this."
+#     return jsonify(message=response)
 
 
-# This needs authentication
+# # This needs authentication
 
 
-@app.route("/api/private")
-@cross_origin(headers=["Content-Type", "Authorization"])
-@requires_auth
-def private():
-    response = "Hello from a private endpoint! You need to be authenticated to see this."
-    return jsonify(message=response)
+# @app.route("/api/private")
+# @cross_origin(headers=["Content-Type", "Authorization"])
+# @requires_auth
+# def private():
+#     response = "Hello from a private endpoint! You need to be authenticated to see this."
+#     return jsonify(message=response)
 
 
 
